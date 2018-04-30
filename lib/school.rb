1d74a896bed2b79c5ecc4dda90804effe58a8240
David Moore <hey@iamdavidmoore.com>
2018-04-30 14:05:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,21 @@
 # code here!
 class School
-  attr_accessor 
-  
+  attr_accessor
+
   def initialize(school)
     @school = school
     @roster = {}
-  end 
+  end
 
-  def add_student=(grade, student_name)
-    @roster[:grade] = student_name
-  end 
+  def add_student=(student_name, grade_int)
+    
+    @roster[grade_int] = student_name
+  end
 
   def student_roster
     @roster
   end
-    
-  
+
+  def grade(grade)
+    #create code to retrieve all students in the grade given
+  end
