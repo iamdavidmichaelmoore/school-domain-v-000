bef4e8004bfac7ca416c2c3abded908a4b36dea4
David Moore <hey@iamdavidmoore.com>
2018-04-30 14:14:11 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,11 @@ class School
 
 
   def add_student=(student_name, grade_int)
-    @roster[grade_int] = student_name
-    
+    if @roster.has_key?(grade_int)
+      @roster[grade_int] << student_name
+    elsif @roster == {} || !(@roster.has_key?(grade_int))
+      @roster[grade_int] = student_name
+
   end
 
   def grade(grade)
